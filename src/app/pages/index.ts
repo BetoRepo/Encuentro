26cc95b65d2198ad3d3ecafb529d148c67fece4e
Freddy Pineda <fpineda@pnaturalyst.local>
2026-09-17 09:59:23 -0400

    Solucion

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,7 +2,7 @@ export { Home } from "./Home";
 export { Inscripcion } from "./Inscripcion";
 export { Consultas } from "./Consultas";
 export { Perfil } from "./Perfil";
-export { Login } from "./Login"; // 👈 Agrega esta línea
+export { Login } from "./Login";
 export { Dashboard } from "./Dashboard";
 export { ChangePassword } from "./ChangePassword";
-export { PanelPrograma }  from "./PanelPrograma";
+export { PanelPrograma } from "./PanelPrograma";
